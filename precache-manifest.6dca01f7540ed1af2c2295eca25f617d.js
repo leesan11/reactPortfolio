d8621ae6efe7f4b5182c3ec7cb6a3b1499e1a4e6
sang <leesanxi@gmail.com>
2019-02-04 15:27:49 -0500

    Updates

---
+++ precache-manifest.6dca01f7540ed1af2c2295eca25f617d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "99ad04dcd3c0cc5d2fd4d26369b42b66",
+    "url": "/reactPortfolio/static/media/createworksheet.99ad04dc.jpg"
+  },
+  {
+    "revision": "e06fab6d2f3fc31039e94ec6b8a401f5",
+    "url": "/reactPortfolio/static/media/background-general.e06fab6d.jpg"
+  },
+  {
+    "revision": "b0bfce8dd94b52aef958",
+    "url": "/reactPortfolio/static/js/runtime~main.b0bfce8d.js"
+  },
+  {
+    "revision": "183d104b68fb8e94a183",
+    "url": "/reactPortfolio/static/js/main.183d104b.chunk.js"
+  },
+  {
+    "revision": "f03e8ae863144a61c620",
+    "url": "/reactPortfolio/static/js/1.f03e8ae8.chunk.js"
+  },
+  {
+    "revision": "183d104b68fb8e94a183",
+    "url": "/reactPortfolio/static/css/main.8ffd1cf2.chunk.css"
+  },
+  {
+    "revision": "7c1714459b2227ec95a7f2edf7c33c4f",
+    "url": "/reactPortfolio/index.html"
+  }
+];
